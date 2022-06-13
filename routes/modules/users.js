b72b6b335b942ff5666a0a6b878bac8babe92d14
wuwac <wuwachon44@gmail.com>
2022-06-13 14:02:53 +0800

    feat: add user registeration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -11,8 +12,27 @@ router.post('/login', (req, res) => {
 router.get('/register', (req, res) => {
   res.render('register')
 })
-router.post('register', (req, res) => {
+router.post('/register', (req, res) => {
   // MongoDB create Data and redirect to '/'
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email })
+    .then(user => {
+      if (!user) return User.create({
+        name,
+        email,
+        password
+      })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+      console.log('User already exists')
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    })
+    .catch(err => console.log(err))
 })
 
 module.exports = router
